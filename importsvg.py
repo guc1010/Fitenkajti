0eea220ddde7f131ce8181a0f33005b28c939662
Kinboise <zsimghuadsjing@163.com>
2022-08-06 11:59:18 +0800

    1692

--- importsvg.py
+++ importsvg.py
@@ -5,7 +5,7 @@ print('自动导入svg至字体。')
 mgk = input('请输入文件夹：')
 
 #ff.loadNamelist('Fitenka.nam')
-lohkengh = 'Fitenkajti.sfd'
+lohkengh = 'FitenkajtiR.sfd'
 
 light = ff.open(lohkengh)
 
@@ -13,21 +13,21 @@ svgs = os.listdir('svg/' + mgk + '/')
 for svg in svgs:
     glyph = light.createChar(int(svg[0:-4], 16))
     glyph.importOutlines('svg/' + mgk + '/' + svg)
-    glyph.left_side_bearing(0)
-    glyph.width(1000)
+    # glyph.left_side_bearing(0)
+    # glyph.width(1000)
     glyph.removeOverlap()
     glyph.correctDirection()
-    glyph.addExtrema(all)
+    glyph.addExtrema('all')
 light.save(lohkengh)
 
-import shutil as sh
+# import shutil as sh
 
-sh.copyfile(lohkengh, 'FitenkajtiR.sfd')
-regular = ff.open('FitenkajtiR.sfd')
-regular.changeWeight(25, 'CJK', 0, 0, 'auto', 1)
-regular.save('FitenkajtiR.sfd')
+# sh.copyfile(lohkengh, 'FitenkajtiR.sfd')
+# regular = ff.open('FitenkajtiR.sfd')
+# regular.changeWeight(25, 'CJK', 0, 0, 'auto', 1)
+# regular.save('FitenkajtiR.sfd')
 
-sh.copyfile(lohkengh, 'FitenkajtiB.sfd')
-bold = ff.open('FitenkajtiB.sfd')
-bold.changeWeight(50, 'CJK', 0, 0, 'auto', 1)
-bold.save('FitenkajtiB.sfd')
+# sh.copyfile(lohkengh, 'FitenkajtiB.sfd')
+# bold = ff.open('FitenkajtiB.sfd')
+# bold.changeWeight(50, 'CJK', 0, 0, 'auto', 1)
+# bold.save('FitenkajtiB.sfd')
